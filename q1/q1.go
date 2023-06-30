7e10d503fd4ed2381814c4ba1ee0049886817e16
miltonpedro <126252785+miltonpedro@users.noreply.github.com>
2023-06-29 22:21:19 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -13,123 +13,20 @@ type Student struct {
 	Age      int
 	Subjects map[string]float64
 }
-	// Seu código aqui
-	package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-)
-
-type Student struct {
-	Name     string
-	Age      int
-	Subjects map[string]float64
-}
-
-func mergeStudentData(studentData1, studentData2 map[string]Student) map[string]Student {
-	mergedData := make(map[string]Student)
-
-	// Percorre os alunos no mapa studentData1
-	for id, student := range studentData1 {
-		mergedData[id] = student
+func MergeStudentData(studentData1 map[string]Student, studentData2 map[string]Student) map[string]Student {
+	result := make(map[string]Student)
+	for key, student := range studentData1 {
+		result[key] = student
 	}
-
-	// Percorre os alunos no mapa studentData2
-	for id, student := range studentData2 {
-		// Verifica se o aluno já existe no mapa mergedData
-		if _, ok := mergedData[id]; ok {
-			// O aluno já existe, atualiza as matérias e notas de acordo com o mapa mais recente
+	for key, student := range studentData2 {
+		if _, exists := result[key]; exists {
 			for subject, grade := range student.Subjects {
-				mergedData[id].Subjects[subject] = grade
+				result[key].Subjects[subject] = grade
 			}
 		} else {
-			// O aluno não existe no mapa mergedData, adiciona-o diretamente
-			mergedData[id] = student
+			result[key] = student
 		}
 	}
-
-	return mergedData
+	return result
 }
-
-func readStudentData() map[string]Student {
-	studentData := make(map[string]Student)
-
-	reader := bufio.NewReader(os.Stdin)
-
-	for {
-		fmt.Print("Digite o ID do aluno (ou 'q' para sair): ")
-		idInput, _ := reader.ReadString('\n')
-		idInput = strings.TrimSpace(idInput)
-
-		if idInput == "q" {
-			break
-		}
-
-		fmt.Print("Digite o nome do aluno: ")
-		nameInput, _ := reader.ReadString('\n')
-		nameInput = strings.TrimSpace(nameInput)
-
-		fmt.Print("Digite a idade do aluno: ")
-		ageInput, _ := reader.ReadString('\n')
-		ageInput = strings.TrimSpace(ageInput)
-		age, _ := strconv.Atoi(ageInput)
-
-		student := Student{
-			Name:     nameInput,
-			Age:      age,
-			Subjects: make(map[string]float64),
-		}
-
-		fmt.Println("Digite as notas das matérias (digite 'q' para sair)")
-
-		for {
-			fmt.Print("Matéria: ")
-			subjectInput, _ := reader.ReadString('\n')
-			subjectInput = strings.TrimSpace(subjectInput)
-
-			if subjectInput == "q" {
-				break
-			}
-
-			fmt.Print("Nota: ")
-			gradeInput, _ := reader.ReadString('\n')
-			gradeInput = strings.TrimSpace(gradeInput)
-			grade, _ := strconv.ParseFloat(gradeInput, 64)
-
-			student.Subjects[subjectInput] = grade
-		}
-
-		studentData[idInput] = student
-	}
-
-	return studentData
-}
-
-func main() {
-	fmt.Println("### Coleta de dados para o primeiro conjunto de alunos ###")
-	studentData1 := readStudentData()
-
-	fmt.Println("\n### Coleta de dados para o segundo conjunto de alunos ###")
-	studentData2 := readStudentData()
-
-	// Mescla os dados dos alunos
-	mergedData := mergeStudentData(studentData1, studentData2)
-
-	// Imprime os dados mesclados
-	for id, student := range mergedData {
-		fmt.Printf("Aluno ID: %s\n", id)
-		fmt.Printf("Nome: %s\n", student.Name)
-		fmt.Printf("Idade: %d\n", student.Age)
-		fmt.Println("Matérias e Notas:")
-		for subject, grade := range student.Subjects {
-			fmt.Printf("%s: %.1f\n", subject, grade)
-		}
-		fmt.Println()
-	}
-}
-
-	return nil
